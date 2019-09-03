d365aa7fcaa213210a37f118b5543fcc6847bc31
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-03 16:57:19 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+
+    @@all = []
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def match(words) #listen.match(["enlists", "google", "inlets", "banana"])
+        words.select{ |word| word.split("").sort == self.word.split("").sort }
+    end
+
+
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+ 
+# binding.pry
+# 'save'
+
+
+# => ["inlets"]
